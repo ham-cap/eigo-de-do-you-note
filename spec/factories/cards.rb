2e70eeb2dda62461250002d3a099e9f911de8e68
Toshiyasu Nonohara <hh0648075@gmail.com>
2024-12-15 12:23:29 +0900

    Cardモデルのテストを作成

--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -1,7 +1,15 @@
 FactoryBot.define do
   factory :card do
-    ja_phrase { "MyText" }
-    en_phrase { "MyText" }
+    ja_phrase { "こんにちは" }
+    en_phrase { "Hello" }
     memorized_at { "2024-12-13 15:33:42" }
+
+    trait :without_ja_phrase do
+      ja_phrase { nil }
+    end
+
+    trait :without_en_phrase do
+      en_phrase { nil }
+    end
   end
 end
