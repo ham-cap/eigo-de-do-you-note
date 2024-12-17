8a4940785a0760ab1ad2c2225cae31ea35ea6c1d
Toshiyasu Nonohara <hh0648075@gmail.com>
2024-12-17 17:54:16 +0900

    new及びcreateアクションを実装

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,4 +2,41 @@ class CardsController < ApplicationController
   def index
     @cards = Card.all
   end
+
+  def new
+    @card = Card.new
+  end
+
+  def create
+    DeepL.configure do |config|
+      config.auth_key = ENV['DEEPL_API_KEY']
+      config.host = 'https://api-free.deepl.com'
+    end
+    original_text = card_params[:original_text]
+    inputted_lang = CLD.detect_language(original_text)
+    translation = DeepL.translate original_text, nil, inputted_lang[:code] == 'ja' ? 'EN' : 'JA'
+
+    if inputted_lang[:code] == 'ja'
+      @card = Card.new(ja_phrase: original_text, en_phrase: translation)
+    else
+      @card = Card.new(ja_phrase: translation, en_phrase: original_text)
+    end
+
+    if @card.save
+      respond_to do |format|
+        format.html { redirect_to cards_path, notice: 'Card was successfully created.' }
+      end
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def card_params
+    params.require(:card).permit(
+      :original_text,
+      :memorized_at
+    )
+  end
 end
