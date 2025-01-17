84aec82ab2bcc61c3560a2e6561836e07bbada4e
Toshiyasu Nonohara <hh0648075@gmail.com>
2025-01-17 17:04:53 +0900

    Userモデルを作成・rails db:migrateを実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_13_063342) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_17_075850) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,14 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_13_063342) do
     t.text "ja_phrase", null: false
     t.text "en_phrase", null: false
     t.datetime "memorized_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "provider", null: false
+    t.string "uid", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
