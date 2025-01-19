5974122f690714d0336fb793bfb26d0924a53243
Toshiyasu Nonohara <hh0648075@gmail.com>
2025-01-19 16:58:53 +0900

    ログイン・ログアウトに関連する処理を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
+  include SessionsHelper
+  before_action :check_logged_in
+
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  def check_logged_in
+    return if current_user
+    redirect_to root_path
+  end
 end
