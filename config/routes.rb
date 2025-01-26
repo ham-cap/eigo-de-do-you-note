cc24e4072856c7eb9b07c56460b7ec93126223d0
Toshiyasu Nonohara <hh0648075@gmail.com>
2025-01-26 11:01:56 +0900

    退会機能を実装した

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
       patch :update_memorized_status
     end
   end
+  resource :user, only: :destroy
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
