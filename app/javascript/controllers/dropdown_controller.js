993727a19077f30784750093f06902677e7ed552
Toshiyasu Nonohara <hh0648075@gmail.com>
2025-01-27 15:24:31 +0900

    退会ボタンをbutton_toからlink_toに戻した
    ・link_toではDELETEメソッドの送信がうまくいかなかったためbutton_toを採用していたが
      メニューの表示を制御するStimulusコントローラー内でevent.stopPropagationを使用して
      いたことにより、Turboによるメソッドの適切な書き換えが阻害されていたことが原因と
      判明したため、該当部分を修正のうえlink_toでの実装に戻した。

---
+++ app/javascript/controllers/dropdown_controller.js
@@ -0,0 +1,3 @@
+import { Dropdown } from "tailwindcss-stimulus-components";
+
+export default class extends Dropdown {}
--- spec/system/cards_spec.rb
+++ spec/system/cards_spec.rb
@@ -159,4 +159,14 @@ RSpec.describe "Cards", type: :system do
     expect(page).to have_content 'Home#index'
     expect(page).to have_content 'ログアウトしました'
   end
+
+  it 'user can withdrawal', :js do
+    click_on 'hamburger_menu_icon'
+    expect(page).to have_content '退会'
+    accept_confirm '退会すると今まで作成したカードは全て削除されます。退会してよろしいですか？' do
+      click_on '退会'
+    end
+    expect(page).to have_content 'Home#index'
+    expect(page).to have_content '退会しました'
+  end
 end
