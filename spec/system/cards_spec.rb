47c9f7edaa87127ea2faa33c56105b9f4722c629
Toshiyasu Nonohara <hh0648075@gmail.com>
2025-01-27 16:11:20 +0900

    CSSの不要な記述を削除・システムテスト内のドロップダウンの表示を確認する箇所を修正

--- spec/system/cards_spec.rb
+++ spec/system/cards_spec.rb
@@ -154,7 +154,7 @@ RSpec.describe "Cards", type: :system do
 
   it 'user can log out', :js do
     click_on 'hamburger_menu_icon'
-    expect(page).to have_selector('.header-dropdown__items', visible: true, wait: 10)
+    expect(page).to have_selector('#menu-open', visible: true, wait: 10)
     expect(page).to(have_content 'ログアウト', wait: 10)
     click_on 'ログアウト'
     expect(page).to have_content 'Home#index'
@@ -163,7 +163,7 @@ RSpec.describe "Cards", type: :system do
 
   it 'user can withdrawal', :js do
     click_on 'hamburger_menu_icon'
-    expect(page).to have_selector('.header-dropdown__items', visible: true, wait: 10)
+    expect(page).to have_selector('#menu-open', visible: true, wait: 10)
     expect(page).to have_content('退会', wait: 10)
     accept_confirm '退会すると今まで作成したカードは全て削除されます。退会してよろしいですか？' do
       click_on '退会'
