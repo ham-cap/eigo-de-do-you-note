9356a2cc0e92e01c061a3a9867ba4faf6ee28960
Toshiyasu Nonohara <hh0648075@gmail.com>
2025-01-28 16:57:51 +0900

    再度プライバシーポリシーのテキストだけをログアウトにして実験

--- spec/system/cards_spec.rb
+++ spec/system/cards_spec.rb
@@ -154,9 +154,9 @@ RSpec.describe "Cards", type: :system do
 
   it 'user can log out', :js do
     click_on 'hamburger_menu_icon'
-    expect(page).to have_content('プライバシーポリシー', wait: 10)
+    expect(page).to have_content('ログアウト', wait: 10)
     within('#menu-open') do
-      click_on 'プライバシーポリシー'
+      click_on 'ログアウト'
     end
     expect(page).to have_content 'Cards#index'
     # expect(page).to have_content 'ログアウトしました'
