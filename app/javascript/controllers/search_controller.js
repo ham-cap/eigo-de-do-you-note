da65d05261fa2501cf86285ef1800604f1d94793
Toshiyasu Nonohara <hh0648075@gmail.com>
2025-01-30 15:01:09 +0900

    インクリメンタルサーチを実装した

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -1,6 +1,6 @@
 import { Controller } from "@hotwired/stimulus";
 
-// Connects to data-controller="search-form"
+// Connects to data-controller="search"
 export default class extends Controller {
   submit() {
     this.element.requestSubmit();
