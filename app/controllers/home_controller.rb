cf56fabd0d416c3f59b6706994969a9615f4ff9c
Toshiyasu Nonohara <hh0648075@gmail.com>
2025-01-31 14:18:36 +0900

    プライバシーポリシーと利用規約のページを作った

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
-  skip_before_action :authenticate, only: :index
+  skip_before_action :authenticate
   def index
+  end
+
+  def terms
+  end
+
+  def privacy
   end
 end
