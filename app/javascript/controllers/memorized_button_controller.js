52dc792d7bf27f2dc7a6c5021ed87df2268457a6
Toshiyasu Nonohara <hh0648075@gmail.com>
2025-02-24 12:23:23 +0900

    覚えたボタンのデザインを変更

--- app/javascript/controllers/memorized_button_controller.js
+++ app/javascript/controllers/memorized_button_controller.js
@@ -5,9 +5,19 @@ export default class extends Controller {
   static targets = ["memorizedButton"];
 
   switchText() {
-    this.memorizedButtonTarget.textContent =
-      this.memorizedButtonTarget.textContent === "覚えた！"
-        ? "忘れた！"
-        : "覚えた！";
+    if (
+      this.memorizedButtonTarget.children[0].alt ===
+      "覚えていない状態のチェックマーク"
+    ) {
+      this.memorizedButtonTarget.innerHTML = "";
+      this.memorizedButtonTarget.innerHTML =
+        '<img src="/assets/check_mark.png" alt="覚えた状態チェックマーク" class="w-10 h-10">';
+      this.memorizedButtonTarget.classList.add("bg-white", "rounded-full");
+    } else {
+      this.memorizedButtonTarget.innerHTML = "";
+      this.memorizedButtonTarget.innerHTML =
+        '<img src="/assets/check_mark_gray.png" alt="覚えていない状態のチェックマーク" class="w-10 h-10">';
+      this.memorizedButtonTarget.classList.add("bg-white", "rounded-full");
+    }
   }
 }
