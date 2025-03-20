a2733d299e2012df67155abeeaddfb2499ef60c6
Toshiyasu Nonohara <hh0648075@gmail.com>
2025-03-20 14:10:24 +0900

    復習モードにおける英文の表示切り替えの処理をシンプル化

--- app/javascript/controllers/review_controller.js
+++ app/javascript/controllers/review_controller.js
@@ -6,13 +6,10 @@ export default class extends Controller {
   switchEnPhraseVisibility() {
     if (this.enPhraseTarget.classList.contains("hidden")) {
       this.enPhraseTarget.classList.remove("hidden");
+      this.switchingButtonTarget.textContent = "英文を隠す";
     } else {
       this.enPhraseTarget.classList.add("hidden");
+      this.switchingButtonTarget.textContent = "英文を表示";
     }
-
-    this.switchingButtonTarget.textContent =
-      this.switchingButtonTarget.textContent === "英文を表示"
-        ? "英文を隠す"
-        : "英文を表示";
   }
 }
