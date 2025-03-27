cc951063cc15e1d765576a77de765baf73f20ea3
Toshiyasu Nonohara <hh0648075@gmail.com>
2025-03-27 09:54:08 +0900

    reviews_controllerに早期リターンを追加・allow_browserのコメントアウトを解除
    ・フレーズが一つも登録されていない状態で復習モードに遷移するとエラーになるため修正した。
    ・開発者ツールでの確認用にコメントアウトしていたallow_browserを有効化した。

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,9 @@
 class ReviewsController < ApplicationController
   def show
     cards = current_user.cards.unmemorized.order(created_at: :desc)
+
+    return @card = nil unless cards.present?
+
     if params[:id] == 'first'
       @card = cards.first
       @next_card = cards.where('id < ?', @card.id).first
